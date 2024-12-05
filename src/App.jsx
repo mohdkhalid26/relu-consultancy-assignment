51ae6fc81990696218e5b0a0f6c2116c75604ec8
mohdkhalid26 <mohdkhalid211003@gmail.com>
2024-12-05 23:23:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./pages/home/page";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/assets/components/suggestion_bar/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SuggestionBar() {
+  return <></>;
+}
+
+export default SuggestionBar;
